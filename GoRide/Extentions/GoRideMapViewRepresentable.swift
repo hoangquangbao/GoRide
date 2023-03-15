235a75ac13f3d43dddfcff960d0efe6f11dc8610
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-03-15 16:08:59 +0700

    Implemented a funtion to update the user's location

--- GoRide/Extentions/GoRideMapViewRepresentable.swift
+++ GoRide/Extentions/GoRideMapViewRepresentable.swift
@@ -14,10 +14,14 @@ struct GoRideMapViewRepresentable: UIViewRepresentable {
     let locationManager = LocationManager()
     
     func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
         mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
-        mapView.userTrackingMode = .follow
-        
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                mapView.userTrackingMode = .follow
+            }
+        }
         return mapView
     }
     
@@ -38,6 +42,21 @@ extension GoRideMapViewRepresentable {
         init(parent: GoRideMapViewRepresentable) {
             self.parent = parent
             super.init()
+        }
+        
+        // Tells the delegate when the map view updates the user's location
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(center:
+                                                CLLocationCoordinate2D(
+                                                    latitude: userLocation.coordinate.latitude,
+                                                    longitude: userLocation.coordinate.longitude),
+                                            span:
+                                                MKCoordinateSpan(
+                                                    latitudeDelta: 0.05,
+                                                    longitudeDelta: 0.05))
+            print("User location: \(region.center)")
+
+            parent.mapView.setRegion(region, animated: true)
         }
     }
 }
--- GoRide/Managers/LocationManager.swift
+++ GoRide/Managers/LocationManager.swift
@@ -12,8 +12,17 @@ class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
     override init() {
+        super.init()
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
     }
 }
