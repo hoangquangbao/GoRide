7fa70f2524970d7a7980e207d5de92e3316a1fbf
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-03-15 10:29:26 +0700

    Added a map to view

---
+++ GoRide/Extentions/GoRideMapViewRepresentable.swift
@@ -0,0 +1,42 @@
+//
+//  MapViewRepresentable.swift
+//  GoRide
+//
+//  Created by Quang Bao on 15/03/2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct GoRideMapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension GoRideMapViewRepresentable {
+    
+    class MapCoordinator: NSObject, MKMapViewDelegate {
+        let parent: GoRideMapViewRepresentable
+        
+        init(parent: GoRideMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+    }
+}
