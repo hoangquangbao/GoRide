bb029d96b9fc9aa431466a4f98a73475b70f3e86
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-03-15 09:44:07 +0700

    Init the project

---
+++ GoRide/App/GoRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoRideApp.swift
+//  GoRide
+//
+//  Created by Quang Bao on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
